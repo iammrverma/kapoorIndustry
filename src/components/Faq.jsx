b2468f87d0af148a2b772421fec610ae157211b8
iammrverma <rajwebz2020@gmail.com>
2024-10-06 03:34:32 +0530

    adding animation to Faq and some edits

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -9,6 +9,7 @@ import PhoneIcon from "@mui/icons-material/Phone";
 import { useState } from "react";
 import { useMediaQuery } from "@mui/material";
 import { useModal } from "./MyModal";
+import { useInView } from "react-intersection-observer";
 
 const faq = [
   {
@@ -24,7 +25,7 @@ const faq = [
     a: "Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros, vitae egestas augue. Duis vel est augue.",
   },
   {
-    q: "Personal data",
+    q: "Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros, vitae egestas augue. Duis vel est augue.",
     a: "Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros, vitae egestas augue. Duis vel est augue.",
   },
   {
@@ -36,6 +37,10 @@ const Faq = () => {
   const [expanded, setExpanded] = useState(false);
   const isLargeScreen = useMediaQuery("(min-width:768px)");
   const { openModal } = useModal();
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
   };
@@ -57,7 +62,7 @@ const Faq = () => {
         }}
       >
         <Brand hideName={true} />
-        <Typography variant="h5">
+        <Typography variant="h3">
           {isLargeScreen ? "Frequently Asked Questions" : "FAQs"}
         </Typography>
       </div>
@@ -68,13 +73,19 @@ const Faq = () => {
             key={index}
             expanded={expanded === `panel${index}`}
             onChange={handleChange(`panel${index}`)}
+            ref={ref}
+            sx={{
+              animation: inView
+                ? `slideUp 0.5s ease ${index * 0.3}s both`
+                : "none",
+            }}
           >
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
               aria-controls={`panel${index}bh-content`}
               id={`panel${index}bh-header`}
             >
-              <Typography sx={{ width: "33%", flexShrink: 0 }}>
+              <Typography sx={{ width: "100%", flexShrink: 0 }}>
                 {faq.q}
               </Typography>
             </AccordionSummary>
@@ -84,12 +95,14 @@ const Faq = () => {
           </Accordion>
         ))}
       </div>
-      <div
-        style={{ display: "flex", justifyContent: "center" }}
-      >
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <Typography align="center">
           Got answer or{" "}
-          <Link role="button" style={{ color: "var(--primary-bg-color)" }} onClick={()=>openModal("query")}>
+          <Link
+            role="button"
+            style={{ color: "var(--primary-bg-color)" }}
+            onClick={() => openModal("query")}
+          >
             Call Executive <PhoneIcon />
           </Link>
         </Typography>
